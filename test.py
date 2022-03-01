c8ce2fa54654c58463bffa24e62aca4f1fa70ace
adrioh <96532321+adrioh@users.noreply.github.com>
2022-03-01 14:59:41 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 def suite_syracuse(n):
     """
     Retourne la suite de Syracuse pour l'entier n.
-
     >>> suite_syracuse(15)
     [15, 46, 23, 70, 35, 106, 53, 160, 80, 40, 20, 10, 5, 16, 8, 4, 2, 1]
     """
@@ -14,36 +13,5 @@ def suite_syracuse(n):
             seq.append(seq[-1] * 3 + 1)
 
     return seq
-
-
-def temps_syracuse(n, altitude=False):
-    """
-    Calcule le temps de vol (éventuellement en altitude) de la suite
-    de Syracuse pour l'entier n.
-
-    >>> temps_syracuse(15)
-    17
-    >>> temps_syracuse(15, altitude=True)
-    10
-    """
-
-    seq = suite_syracuse(n)
-    if not altitude:            # Temps de vol total
-        return len(seq) - 1
-    else:                       # Temps de vol en altitude
-        # Construction de la séquence en altitude
-        alt = []
-        for i in seq:
-            if i >= n:
-                alt.append(i)
-            else:
-                break
-        return len(alt) - 1
-
-if __name__ == '__main__':
-
-    n = 15
-    print("Suite de Syracuse pour n =", n)
-    print(suite_syracuse(n))
-    print("Temps de vol total:      ", temps_syracuse(n))
-    print("Temps de vol en altitude :", temps_syracuse(n, altitude=True))
+n = int(input("entrer nombre sup à 1 : "))
+print(suite_syracuse(n))
